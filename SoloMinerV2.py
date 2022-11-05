24cbf23b4201b29a3a0167ef6d0a102d7212abcf
Aditio3 <117493120+Aditio3@users.noreply.github.com>
2022-11-05 19:46:04 +0700

    Solominerv2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = '19bykC6Bn2KWLkG3mE6DbNcZGtMtN54A4W' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
